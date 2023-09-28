349a5f16e734927be2cea1d542cb8d0152a2ef9e
Luciano Kohler da Silva <lucianokohlerdasilva@gmail.com>
2023-09-28 16:25:49 -0300

    criado header e footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+import { Container } from "react-bootstrap";
+
+export default function Footer() {
+  return (
+    <div className="bg-body-tertiary text-center py-3">
+        <p className="text-muted mb-0 text-danger">BootsGallery &copy; 2023 - SENAC</p>
+    </div>
+    )
+
+}
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,21 @@
-export default function Header(){
-  return(
-    <h1>Header</h1>
-  )
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export default function Header() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary px-3">
+        <Navbar.Brand href="#home">BootsGallery</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="flex-grow-1">
+            <Nav.Link href="#features">Aquarela</Nav.Link>
+            <Nav.Link href="#pricing">Pixel Arts</Nav.Link>
+            <Nav.Link href="#pricing">Fotografias</Nav.Link>
+          </Nav>
+          <Nav className='justify-content-end'>
+            <Nav.Link disabled>Olhe, Aprecie, Inspire-se!</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+  );
 }
